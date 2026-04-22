ae40a36fae7d6ebeef7d5444479742907b38f5dd
Andrew <14799876+solanyn@users.noreply.github.com>
2026-04-22 16:10:26 +1000

    scrib v0.3.1: fix Ctrl+C upload pipeline + version string

--- Formula/scrib.rb
+++ Formula/scrib.rb
@@ -1,17 +1,17 @@
 class Scrib < Formula
   desc "Voice-to-text transcription tool with local and remote modes"
   homepage "https://github.com/solanyn/mono/tree/main/scrib"
-  version "2026.04.591"
+  version "2026.04.592"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/solanyn/mono/releases/download/scrib/v0.3.0/scrib-darwin-arm64"
-      sha256 "cb407956d2b4cd26ee775be14aa441d35dcbcf6f98057a49d2dc447a2e33cee4"
+      url "https://github.com/solanyn/mono/releases/download/scrib/v0.3.1/scrib-darwin-arm64"
+      sha256 "84e0e730fbdc495aff5a1c9adffedb51fd3e5971fcb2ba0fcf2122235723ee16"
     end
     on_intel do
-      url "https://github.com/solanyn/mono/releases/download/scrib/v0.3.0/scrib-darwin-amd64"
-      sha256 "cfe9299322155bd62e24f1d40a9e73545b28ddd35a956d869eab5d1621e1a217"
+      url "https://github.com/solanyn/mono/releases/download/scrib/v0.3.1/scrib-darwin-amd64"
+      sha256 "65f6e56c6d1b3fc36f898063e1b2e503e89335758b46128ec3a9aec6e49637ed"
     end
   end
 
